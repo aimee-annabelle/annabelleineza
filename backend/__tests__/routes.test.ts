d8c561c785c43233f700f5deeadb2d40375de6f5
aimeeannabelle <inezannabelle@gmail.com>
2022-03-24 17:06:22 +0200

    last git actions

--- backend/__tests__/routes.test.ts
+++ backend/__tests__/routes.test.ts
@@ -2,24 +2,31 @@
 import request from 'supertest'
 import app from '../src/index'
 describe('Post Endpoints', () => {
+
   it('Get Token Test', async () => {
+
     const res = await request(app)
       .post('/api/token/get')
       .send({
         amount:900,
-        meter_number:122258
+        meter_number:185358
         
     })
+
     expect(res.statusCode).toEqual(201)
+    
   })
+
   it('Not Add Because Amount is invalid Test', async () => {
+
     const res = await request(app)
       .post('/api/token/get')
       .send({
         amount:550,
-        meter_number:128856
+        meter_number:122656
         
     })
+
     expect(res.statusCode).toEqual(409)
   })
 })
--- backend/__tests__/unit.test.ts
+++ backend/__tests__/unit.test.ts
@@ -1,23 +1,31 @@
 // Unit Test
 import moment from 'moment'
+
 import {calculateExpirationDate} from '../src/helpers/calculations'
+
 describe('Post Endpoints', () => {
+
   it('Return Expiration Date Test', async () => {
+
     const expiration_date = calculateExpirationDate(400)
+    
     const expected_date = moment().subtract(4, 'days').add(1,'second').toDate()
     expect(expiration_date).toBe(expiration_date)
+
   })
   it('Return False Amount is invalid', async () => {
+
     const expiration_date = calculateExpirationDate(450)
     expect(expiration_date).toBeFalsy()
+    
   })
-  it('Token Expiration', async () => {
-    const expiration_date = calculateExpirationDate(400)
-    const expected_date = moment().subtract(4, 'days').add(1,'second').toDate()
-    expect(expiration_date).toBe(expiration_date)
-  })
-  it('Invalidity', async () => {
-    const expiration_date = calculateExpirationDate(450)
-    expect(expiration_date).toBeFalsy()
-  })
+  // it('Expiration Date Test', async () => {
+  //   const expiration_date = calculateExpirationDate(400)
+  //   const expected_date = moment().subtract(4, 'days').add(1,'second').toDate()
+  //   expect(expiration_date).toBe(expiration_date)
+  // })
+  // it('Invalidity', async () => {
+  //   const expiration_date = calculateExpirationDate(450)
+  //   expect(expiration_date).toBeFalsy()
+  // })
 })
